411e8d0a4203c5a20ab2c1c44d4362e70ef96eb2
gary <gazzok@gmail.com>
2025-10-22 21:48:43 +0100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformbucketgk" {
+  bucket = "terraformbucketgk"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformbucketgk"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
